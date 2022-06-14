faee3f2cd419725721f7b8e74d2d0c708ab3a793
Marcos Piñero <marcos.pinero1976@gmail.com>
2022-06-14 16:39:02 -0300

    Varias modificaciones

---
+++ app/Persona.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+class Persona extends Model
+{
+    protected $fillable = ['nombre', 'apellido','email','telefono','ciudad','observaciones','tipoDocumento','documento','nacimiento','peso','altura','foto','fallecimiento'];
+
+    public function jugador()
+    {
+        return $this->hasOne('App\Jugador');
+    }
+
+    public function tecnico()
+    {
+        return $this->hasOne('App\Tecnico');
+    }
+
+    public function arbitro()
+    {
+        return $this->hasOne('App\Arbitro');
+    }
+
+    public function getFullNameAttribute()
+    {
+        return $this->apellido . ', ' . $this->nombre;
+    }
+
+    public function getFullNameAgeAttribute()
+    {
+        return $this->apellido . ', ' . $this->nombre.' ('.$this->getAgeAttribute().')';
+    }
+
+    public function getAgeAttribute()
+    {
+        if (!is_null($this->fallecimiento))
+        {
+            return Carbon::parse($this->nacimiento)->diff(Carbon::parse($this->fallecimiento))->format('%y').' años ('.date('d/m/Y', strtotime($this->nacimiento)).'-'.date('d/m/Y', strtotime($this->fallecimiento)).')';
+        }
+        if (!is_null($this->nacimiento))
+        {
+            return Carbon::parse($this->nacimiento)->age.' años';
+        }
+
+    }
+}
