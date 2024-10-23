066cafc0a10a3faee9a67f925d684719ec7fd2e7
Marcos Piñero <marcos.pinero1976@gmail.com>
2024-10-23 08:37:20 -0300

    varios

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        ini_set('memory_limit', '256M');
     }
 
     /**
--- resources/views/layouts/partials/headerPublic.blade.php
+++ resources/views/layouts/partials/headerPublic.blade.php
@@ -316,100 +316,101 @@
 
             </ul>
         </div>
-    </div>
+        <!-- Menú horizontal -->
+        @if(Session::has('codigoTorneo'))
+            <div class="hamburger-menu" id="hamburgerSubMenu">
+                <span></span>
+                <span></span>
+                <span></span>
 
-    <!-- Menú horizontal -->
-    @if(Session::has('codigoTorneo'))
-        <div class="hamburger-menu" id="hamburgerSubMenu">
-            <span></span>
-            <span></span>
-            <span></span>
+            </div>
+            <span id="spanTorneo" style="background-color: #ccf1cd;"><a href="#" style="color: #4ea3e7;font-weight: bold;">{{Session::get('nombreTorneo')}}</a></span>
+            <!-- Menú móvil -->
+            <div class="mobile-menu" id="mobileSubMenu">
+                <ul>
 
-        </div>
-        <span id="spanTorneo" style="background-color: #ccf1cd;"><a href="#" style="color: #4ea3e7;font-weight: bold;">{{Session::get('nombreTorneo')}}</a></span>
-        <!-- Menú móvil -->
-        <div class="mobile-menu" id="mobileSubMenu">
-            <ul>
+                    <li><a class="dropdown-item" href="{{route('fechas.ver',  array('torneoId' => Session::get('codigoTorneo')))}}">Fixture</a></li>
+                    @if(Session::has('sessionPosiciones'))
+                        <li class="dropdown">
 
-                <li><a class="dropdown-item" href="{{route('fechas.ver',  array('torneoId' => Session::get('codigoTorneo')))}}">Fixture</a></li>
-                @if(Session::has('sessionPosiciones'))
+                            <a class="dropdown-item" href="#">Tablas <span class="toggle">+</span></a>
+
+                            <ul class="sub-dropdown-menu" role="menu" style="display: none">
+                                @if(Session::has('sessionAcumulado'))
+                                    <li><a class="dropdown-item" href="{{route('torneos.acumulado',  array('torneoId' => Session::get('codigoTorneo')))}}">Acumulado</a></li>
+                                @endif
+                                @if(Session::has('sessionPaenza'))
+                                    <li><a class="dropdown-item" href="{{route('grupos.metodo',  array('torneoId' => Session::get('codigoTorneo')))}}">Método Paenza</a></li>
+                                @endif
+                                <li><a class="dropdown-item" href="{{route('grupos.posicionesPublic',  array('torneoId' => Session::get('codigoTorneo')))}}">Posiciones</a></li>
+
+                                @if(Session::has('sessionPromedios'))
+                                    <li><a class="dropdown-item" href="{{route('torneos.promediosPublic',  array('torneoId' => Session::get('codigoTorneo')))}}">Promedios</a></li>
+                                @endif
+                            </ul>
+                        </li>
+                    @endif
                     <li class="dropdown">
 
-                        <a class="dropdown-item" href="#">Tablas <span class="toggle">+</span></a>
-
+                        <a class="dropdown-item" href="#">{{ __('Protagonistas') }} <span class="toggle">+</span></a>
                         <ul class="sub-dropdown-menu" role="menu" style="display: none">
-                            @if(Session::has('sessionAcumulado'))
-                                <li><a class="dropdown-item" href="{{route('torneos.acumulado',  array('torneoId' => Session::get('codigoTorneo')))}}">Acumulado</a></li>
-                            @endif
-                            @if(Session::has('sessionPaenza'))
-                                <li><a class="dropdown-item" href="{{route('grupos.metodo',  array('torneoId' => Session::get('codigoTorneo')))}}">Método Paenza</a></li>
-                            @endif
-                            <li><a class="dropdown-item" href="{{route('grupos.posicionesPublic',  array('torneoId' => Session::get('codigoTorneo')))}}">Posiciones</a></li>
+                            <li><a class="dropdown-item" href="{{route('grupos.arqueros',  array('torneoId' => Session::get('codigoTorneo')))}}">Arqueros</a></li>
+                            <li><a class="dropdown-item" href="{{route('grupos.goleadoresPublic',  array('torneoId' => Session::get('codigoTorneo')))}}">Goleadores</a></li>
+                            <li><a class="dropdown-item" href="{{route('grupos.jugadores',  array('torneoId' => Session::get('codigoTorneo')))}}">Jugadores</a></li>
+                            <li><a class="dropdown-item" href="{{route('grupos.tarjetasPublic',  array('torneoId' => Session::get('codigoTorneo')))}}">Tarjetas</a></li>
 
-                            @if(Session::has('sessionPromedios'))
-                                <li><a class="dropdown-item" href="{{route('torneos.promediosPublic',  array('torneoId' => Session::get('codigoTorneo')))}}">Promedios</a></li>
-                            @endif
+                            <li><a class="dropdown-item" href="{{route('grupos.tecnicos',  array('torneoId' => Session::get('codigoTorneo')))}}">Técnicos</a></li>
                         </ul>
                     </li>
-                @endif
-                <li class="dropdown">
+                    <li><a class="dropdown-item" href="{{route('torneos.estadisticasTorneo',  array('torneoId' => Session::get('codigoTorneo')))}}">Estadísticas</a></li>
 
-                    <a class="dropdown-item" href="#">{{ __('Protagonistas') }} <span class="toggle">+</span></a>
-                    <ul class="sub-dropdown-menu" role="menu" style="display: none">
-                        <li><a class="dropdown-item" href="{{route('grupos.arqueros',  array('torneoId' => Session::get('codigoTorneo')))}}">Arqueros</a></li>
-                        <li><a class="dropdown-item" href="{{route('grupos.goleadoresPublic',  array('torneoId' => Session::get('codigoTorneo')))}}">Goleadores</a></li>
-                        <li><a class="dropdown-item" href="{{route('grupos.jugadores',  array('torneoId' => Session::get('codigoTorneo')))}}">Jugadores</a></li>
-                        <li><a class="dropdown-item" href="{{route('grupos.tarjetasPublic',  array('torneoId' => Session::get('codigoTorneo')))}}">Tarjetas</a></li>
+                    <li><a class="dropdown-item" href="{{route('torneos.plantillas',  array('torneoId' => Session::get('codigoTorneo')))}}">Plantillas</a></li>
+                </ul>
+            </div>
+            <div id="horizontalMenu" class="navbar-collapse collapse">
+                <ul>
+                    <li><a href="#" style="color: #4ea3e7;font-weight: bold;">{{Session::get('nombreTorneo')}}</a></li>
+                    <li><a class="dropdown-item" href="{{route('fechas.ver',  array('torneoId' => Session::get('codigoTorneo')))}}">Fixture</a></li>
+                    @if(Session::has('sessionPosiciones'))
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ __('Tablas') }} <span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu" style="display: none">
+                                @if(Session::has('sessionAcumulado'))
+                                    <li><a class="dropdown-item" href="{{route('torneos.acumulado',  array('torneoId' => Session::get('codigoTorneo')))}}">Acumulado</a></li>
+                                @endif
+                                @if(Session::has('sessionPaenza'))
+                                    <li><a class="dropdown-item" href="{{route('grupos.metodo',  array('torneoId' => Session::get('codigoTorneo')))}}">Método Paenza</a></li>
+                                @endif
+                                <li><a class="dropdown-item" href="{{route('grupos.posicionesPublic',  array('torneoId' => Session::get('codigoTorneo')))}}">Posiciones</a></li>
 
-                        <li><a class="dropdown-item" href="{{route('grupos.tecnicos',  array('torneoId' => Session::get('codigoTorneo')))}}">Técnicos</a></li>
-                    </ul>
-                </li>
-                <li><a class="dropdown-item" href="{{route('torneos.estadisticasTorneo',  array('torneoId' => Session::get('codigoTorneo')))}}">Estadísticas</a></li>
+                                @if(Session::has('sessionPromedios'))
+                                    <li><a class="dropdown-item" href="{{route('torneos.promediosPublic',  array('torneoId' => Session::get('codigoTorneo')))}}">Promedios</a></li>
+                                @endif
+                            </ul>
+                        </li>
+                    @endif
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ __('Protagonistas') }} <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu" style="display: none">
+                            <li><a class="dropdown-item" href="{{route('grupos.arqueros',  array('torneoId' => Session::get('codigoTorneo')))}}">Arqueros</a></li>
+                            <li><a class="dropdown-item" href="{{route('grupos.goleadoresPublic',  array('torneoId' => Session::get('codigoTorneo')))}}">Goleadores</a></li>
+                            <li><a class="dropdown-item" href="{{route('grupos.jugadores',  array('torneoId' => Session::get('codigoTorneo')))}}">Jugadores</a></li>
+                            <li><a class="dropdown-item" href="{{route('grupos.tarjetasPublic',  array('torneoId' => Session::get('codigoTorneo')))}}">Tarjetas</a></li>
 
-                <li><a class="dropdown-item" href="{{route('torneos.plantillas',  array('torneoId' => Session::get('codigoTorneo')))}}">Plantillas</a></li>
-            </ul>
-        </div>
-        <div id="horizontalMenu" class="navbar-collapse collapse">
-            <ul>
-                <li><a href="#" style="color: #4ea3e7;font-weight: bold;">{{Session::get('nombreTorneo')}}</a></li>
-                <li><a class="dropdown-item" href="{{route('fechas.ver',  array('torneoId' => Session::get('codigoTorneo')))}}">Fixture</a></li>
-                @if(Session::has('sessionPosiciones'))
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ __('Tablas') }} <span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu" style="display: none">
-                        @if(Session::has('sessionAcumulado'))
-                        <li><a class="dropdown-item" href="{{route('torneos.acumulado',  array('torneoId' => Session::get('codigoTorneo')))}}">Acumulado</a></li>
-                        @endif
-                            @if(Session::has('sessionPaenza'))
-                        <li><a class="dropdown-item" href="{{route('grupos.metodo',  array('torneoId' => Session::get('codigoTorneo')))}}">Método Paenza</a></li>
-                            @endif
-                        <li><a class="dropdown-item" href="{{route('grupos.posicionesPublic',  array('torneoId' => Session::get('codigoTorneo')))}}">Posiciones</a></li>
+                            <li><a class="dropdown-item" href="{{route('grupos.tecnicos',  array('torneoId' => Session::get('codigoTorneo')))}}">Técnicos</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="dropdown-item" href="{{route('torneos.estadisticasTorneo',  array('torneoId' => Session::get('codigoTorneo')))}}">Estadísticas</a></li>
 
-                            @if(Session::has('sessionPromedios'))
-                        <li><a class="dropdown-item" href="{{route('torneos.promediosPublic',  array('torneoId' => Session::get('codigoTorneo')))}}">Promedios</a></li>
-                            @endif
-                    </ul>
-                </li>
-                @endif
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ __('Protagonistas') }} <span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu" style="display: none">
-                        <li><a class="dropdown-item" href="{{route('grupos.arqueros',  array('torneoId' => Session::get('codigoTorneo')))}}">Arqueros</a></li>
-                        <li><a class="dropdown-item" href="{{route('grupos.goleadoresPublic',  array('torneoId' => Session::get('codigoTorneo')))}}">Goleadores</a></li>
-                        <li><a class="dropdown-item" href="{{route('grupos.jugadores',  array('torneoId' => Session::get('codigoTorneo')))}}">Jugadores</a></li>
-                        <li><a class="dropdown-item" href="{{route('grupos.tarjetasPublic',  array('torneoId' => Session::get('codigoTorneo')))}}">Tarjetas</a></li>
-
-                        <li><a class="dropdown-item" href="{{route('grupos.tecnicos',  array('torneoId' => Session::get('codigoTorneo')))}}">Técnicos</a></li>
-                    </ul>
-                </li>
-                <li><a class="dropdown-item" href="{{route('torneos.estadisticasTorneo',  array('torneoId' => Session::get('codigoTorneo')))}}">Estadísticas</a></li>
-
-                <li><a class="dropdown-item" href="{{route('torneos.plantillas',  array('torneoId' => Session::get('codigoTorneo')))}}">Plantillas</a></li>
+                    <li><a class="dropdown-item" href="{{route('torneos.plantillas',  array('torneoId' => Session::get('codigoTorneo')))}}">Plantillas</a></li>
 
 
-            </ul>
-        </div>
-    @endif
+                </ul>
+            </div>
+        @endif
+    </div>
+
+
 
     <script>
         function showHorizontalMenu(torneoId) {
