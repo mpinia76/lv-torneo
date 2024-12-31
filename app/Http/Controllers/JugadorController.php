e1e9ccca7a058fadb8461c8ca43241e2b7fc1e53
mpinia76 <marcos.pinero1976@gmail.com>
2024-12-31 17:44:35 -0300

    Varias modificaciones

--- app/Http/Controllers/JugadorController.php
+++ app/Http/Controllers/JugadorController.php
@@ -320,7 +320,7 @@ ORDER BY torneos.year DESC, torneos.id DESC';
         $titulosJugadorLiga=0;
         $titulosJugadorInternacional=0;
         foreach ($torneosJugador as $torneo){
-            $torneo->escudoNombre = $torneo->escudo;
+            $torneo->escudoTorneo = $torneo->escudo;
             $grupos = Grupo::where('torneo_id', '=',$torneo->idTorneo)->get();
             $arrgrupos='';
             foreach ($grupos as $grupo){
