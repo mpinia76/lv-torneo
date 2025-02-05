a6b7ad1370074f20065b96c7f7f3848a69c3d745
Marcos Piñero <marcos.pinero1976@gmail.com>
2025-02-05 10:46:11 -0300

    varios

--- resources/views/plantillas/create.blade.php
+++ resources/views/plantillas/create.blade.php
@@ -47,13 +47,7 @@
             </thead>
 
             <tbody id="cuerpoJugador">
-            <tr>
-                <td></td>
-                <td>{{ Form::select('jugador[]',[], '',['class' => 'form-control js-example-basic-single', 'style' => 'width: 300px']) }}</td>
-                <td>{{Form::number('dorsal[]', '', ['class' => 'form-control', 'style' => 'width:70px;'])}}</td>
 
-                <td><a href="#" class="btn btn-danger remove"><i class="glyphicon glyphicon-remove"></i></a></td>
-            </tr>
 
             </tbody>
 
