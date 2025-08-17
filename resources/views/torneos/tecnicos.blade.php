5e8be17469eff814fc16aedb7e9e1a59ea49a05c
marcoscodnet <81051772+marcoscodnet@users.noreply.github.com>
2025-08-17 11:36:51 -0300

    Varias modificaciones

--- resources/views/torneos/tecnicos.blade.php
+++ resources/views/torneos/tecnicos.blade.php
@@ -116,7 +116,7 @@
                                     <a href="{{route('equipos.ver', array('equipoId' => $escudoArr[1]))}}" >
                                         <img id="original" src="{{ url('images/'.$escudoArr[0]) }}" height="25">
                                     </a>
-                                    ({{$escudoArr[2]}} pts.) ({{$escudoArr[3]}}%)
+                                    ({{$escudoArr[2]}} pts.) ({{$escudoArr[3]}})
                                     @if(isset($escudoArr[4]) && $escudoArr[4] != '')
                                         <!-- Mostrar datos adicionales de $escudoArr[2] aquí -->
                                         {{  $escudoArr[4] }}
