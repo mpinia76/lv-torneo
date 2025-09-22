8a011685362a581ce56a34884a5d0be4370d74c4
Marcos Piñero <marcos.pinero1976@gmail.com>
2025-09-22 08:45:47 -0300

    varios

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,16 +1,3 @@
-{{-- Footer --}}
-<footer class="bg-dark text-white mt-auto py-3">
-    <div class="container d-flex flex-column flex-md-row justify-content-between align-items-center">
-        <div>
-            &copy; {{ date('Y') }} Todos los derechos reservados.
-        </div>
-        <!--<div>
-            <a href="#" class="text-white text-decoration-none me-3">Aviso Legal</a>
-            <a href="#" class="text-white text-decoration-none me-3">Privacidad</a>
-            <a href="#" class="text-white text-decoration-none">Contacto</a>
-        </div>-->
-    </div>
-</footer>
 <!-- JavaScripts -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
