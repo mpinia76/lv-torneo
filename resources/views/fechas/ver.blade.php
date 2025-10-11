daed3dc2da86cb810b6bf9a9b6fe00bc98844ebc
marcoscodnet <81051772+marcoscodnet@users.noreply.github.com>
2025-10-11 10:19:42 -0300

    Varias modificaciones

--- resources/views/fechas/ver.blade.php
+++ resources/views/fechas/ver.blade.php
@@ -169,15 +169,19 @@
             background-color: #e6ffe6 !important;
         }
         .table tbody tr td:nth-child(1) { /* Hora */
-            width: 10%;
+            width: 5%;
         }
-        .table tbody tr td:nth-child(2), /* Local */
+        .table tbody tr td:nth-child(2){ /* Local */
+
+            width: 20%;
+        }
+
         .table tbody tr td:nth-child(5) { /* Visitante */
             width: 35%;
         }
         .table tbody tr td:nth-child(3), /* Goles local */
         .table tbody tr td:nth-child(4) { /* Goles visitante */
-            width: 10%;
+            width: 2%;
             text-align: center;
         }
     </style>
