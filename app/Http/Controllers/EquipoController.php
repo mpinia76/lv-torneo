ef69372c6e0aaaab7ee7f8555b6c44f5a874b8ad
marcoscodnet <81051772+marcoscodnet@users.noreply.github.com>
2025-11-23 20:58:50 -0300

    Varias modificaciones

--- app/Http/Controllers/EquipoController.php
+++ app/Http/Controllers/EquipoController.php
@@ -542,7 +542,7 @@ INNER JOIN posicion_torneos ON posicion_torneos.torneo_id=grupos.torneo_id AND p
 WHERE plantillas.equipo_id='.$id.'
 GROUP BY jugadors.id,personas.foto,personas.apellido,personas.nombre';
 
-        $sql.=' UNION ALL
+        /*$sql.=' UNION ALL
 SELECT
     jugadors.id AS jugador_id,
     personas.foto,
@@ -569,7 +569,7 @@ INNER JOIN titulo_torneos
     ON titulo_torneos.titulo_id = titulos.id
 
 WHERE plantillas.equipo_id = '.$id.'
-GROUP BY jugadors.id, personas.foto, personas.apellido, personas.nombre';
+GROUP BY jugadors.id, personas.foto, personas.apellido, personas.nombre';*/
         $sql .=' ) as subconsulta
 
 group by jugador_id,jugador, foto
