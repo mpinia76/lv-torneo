0d23d2464f256222278b134b5a5ad791a5384d2c
Marcos Piñero <marcos.pinero1976@gmail.com>
2026-02-20 07:52:06 -0300

    varios

--- app/Http/Controllers/FechaController.php
+++ app/Http/Controllers/FechaController.php
@@ -5943,7 +5943,7 @@ private function normalizarMinuto(string $texto): int
 
                         $cacheKey = 'slug_jugador_' . $alineacion->jugador->id;
                         $cachedUrl = Cache::get($cacheKey);
-                        //Log::info('Cache get', ['key' => $cacheKey, 'url' => $cachedUrl]);
+                        Log::info('Cache get', ['key' => $cacheKey, 'url' => $cachedUrl]);
 
                         $html2 = null;
 
