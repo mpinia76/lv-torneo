bd98668b170c03e1441304d1f8d5f1c7ca3ff651
marcoscodnet <81051772+marcoscodnet@users.noreply.github.com>
2026-02-24 21:16:08 -0300

    Varias modificaciones

--- resources/views/plantillas/reasignar_grupo.blade.php
+++ resources/views/plantillas/reasignar_grupo.blade.php
@@ -1,31 +1,51 @@
 @extends('layouts.app')
 
-@section('pageTitle', 'Importar jugador')
+@section('pageTitle', 'Reasignar grupo')
 
 @section('content')
 <div class="container">
     <h3>Reasignar grupo</h3>
 
-    <p><strong>Equipo:</strong> {{ $plantilla->equipo->nombre }}</p>
+    <div class="container">
+        <h1 class="display-6">Importar Plantilla de {{$plantilla->equipo->nombre}} en {{$grupo->torneo->nombre}} {{$grupo->torneo->year}}</h1>
+        <br>
+        @if($plantilla->equipo->escudo)
+            <img id="original" src="{{ url('images/'.$plantilla->equipo->escudo) }}" height="50">
+        @endif
+
+        <hr/>
+
+        <!-- if validation in the controller fails, show the errors -->
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
 
     <form method="POST" action="{{ route('plantillas.guardarGrupo', $plantilla->id) }}">
         @csrf
 
-        <div class="mb-3">
-            <label>Grupo</label>
-            <select name="grupo_id" class="form-control">
-                @foreach($grupos as $id => $nombre)
-                    <option value="{{ $id }}"
-                            @if($plantilla->grupo_id == $id) selected @endif>
-                        {{ $nombre }}
-                    </option>
-                @endforeach
-            </select>
+        <div class="row">
+            <div class="form-group col-xs-12 col-sm-6 col-md-2">
+                <label>Grupo</label>
+                <select name="grupo_id" class="form-control">
+                    @foreach($grupos as $id => $nombre)
+                        <option value="{{ $id }}"
+                                @if($plantilla->grupo_id == $id) selected @endif>
+                            {{ $nombre }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
         </div>
 
         <button class="btn btn-primary">Guardar</button>
 
-        <a href="{{ url()->previous() }}" class="btn btn-secondary">Cancelar</a>
+        <a href="{{route('plantillas.edit', $plantilla->id)}}" class="btn btn-success m-1">Volver</a>
     </form>
 </div>
 @endsection
